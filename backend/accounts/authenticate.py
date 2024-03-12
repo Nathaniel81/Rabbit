d621a9bc145c6ca93a955d8a4db4c173772fd5bf
Nathaniel81 <nathanield356@gmail.com>
2024-03-12 16:50:54 +0300

    Update CSRF validation enforcement method

--- backend/accounts/authenticate.py
+++ backend/accounts/authenticate.py
@@ -1,6 +1,7 @@
 from rest_framework_simplejwt import authentication as jwt_authentication
 from django.conf import settings
 from rest_framework import authentication, exceptions as rest_exceptions
+from django.middleware.csrf import CsrfViewMiddleware
 
 
 def enforce_csrf(request):
@@ -8,10 +9,12 @@ def enforce_csrf(request):
     Enforce CSRF validation.
     """
 
-    check = authentication.CSRFCheck(request)
-    reason = check.process_view(request, None, (), {})
+    csrf_middleware = CsrfViewMiddleware(request)
+    csrf_token = request.META.get('HTTP_X_CSRFTOKEN', '')
+    request.META['CSRF_COOKIE'] = csrf_token
+    reason = csrf_middleware.process_view(request, None, (), {})
     if reason:
-      raise rest_exceptions.PermissionDenied('CSRF Failed: %s' % reason)
+        raise rest_exceptions.PermissionDenied('CSRF Failed: %s' % reason)
 
 class CustomAuthentication(jwt_authentication.JWTAuthentication):
     """
@@ -19,8 +22,10 @@ class CustomAuthentication(jwt_authentication.JWTAuthentication):
     """
 
     def authenticate(self, request):
-        header = self.get_header(request)
-        raw_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE']) or None 
+        raw_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE']) or None
+
+        if raw_token is None:
+            return None
         validated_token = self.get_validated_token(raw_token)
         enforce_csrf(request)
 
