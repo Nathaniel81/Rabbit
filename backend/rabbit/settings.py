58e0666108a79bc2889d7a733d6db74f56dd5baa
Nathaniel81 <nathanield356@gmail.com>
2024-03-19 10:14:03 +0300

    Implement pagination and infinite scroll

--- backend/rabbit/settings.py
+++ backend/rabbit/settings.py
@@ -176,18 +176,18 @@ AUTH_USER_MODEL = 'accounts.User'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-database_url = os.getenv('DATABASE_URL')
-
 DATABASES = {
-    'default': dj_database_url.parse(database_url)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# database_url = os.getenv('DATABASE_URL')
+
+# DATABASES = {
+#     'default': dj_database_url.parse(database_url)
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
