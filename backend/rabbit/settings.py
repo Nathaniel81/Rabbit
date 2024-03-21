6ce7c116995df4cac793dbaa896983e8f78e30fe
Nathaniel81 <nathanield356@gmail.com>
2024-03-21 21:54:56 +0300

    Update Redis location configuration

--- backend/rabbit/settings.py
+++ backend/rabbit/settings.py
@@ -126,8 +126,8 @@ REDIS_URL = os.getenv('REDIS_URL')
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        # 'LOCATION': REDIS_URL,
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': REDIS_URL,
+        # 'LOCATION': 'redis://127.0.0.1:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
