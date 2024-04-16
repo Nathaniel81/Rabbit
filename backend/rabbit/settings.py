5d46a42448e4c044455ce0ab16f2d863b8825618
Nathaniel81 <nathanield356@gmail.com>
2024-04-16 09:00:43 +0300

    Update settings.py

--- backend/rabbit/settings.py
+++ backend/rabbit/settings.py
@@ -141,8 +141,8 @@ CACHES = {
 # }
 
 SIMPLE_JWT = {
-  'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
-  'REFRESH_TOKEN_LIFETIME': timedelta(minutes=60),
+  'ACCESS_TOKEN_LIFETIME': timedelta(seconds=3000),
+  'REFRESH_TOKEN_LIFETIME': timedelta(minutes=6000),
   'ROTATE_REFRESH_TOKENS': False,
   'BLACKLIST_AFTER_ROTATION': True,
   'UPDATE_LAST_LOGIN': False,
