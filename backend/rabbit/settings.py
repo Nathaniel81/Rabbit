9b3692815931a84750c47797e58f12b172b3d72c
Nathaniel81 <nathanield356@gmail.com>
2024-05-27 16:36:11 +0300

    Add CORS and CSRF configurations for secure cross-origin requests

--- backend/rabbit/settings.py
+++ backend/rabbit/settings.py
@@ -33,17 +33,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['rabbitapi.onrender.com']
 
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://rabbit-23mo.onrender.com"
 ]
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = True
 CSRF_TRUSTED_ORIGINS = [
     'https://rabbit-23mo.onrender.com',
 ]
